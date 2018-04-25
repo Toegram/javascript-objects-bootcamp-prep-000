32f9ea6cd0f4a6f1f7968b736728dd3f1f9cc5cf
Anthony Turetsky <tturetsky@gmail.com>
2018-04-25 21:49:37 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -36,6 +36,6 @@ function updatePlaylist(playlist, artistName, songTitle) {
 function removeFromPlaylist(playlist, artistName) {
     delete playlist.artistName;
     return playlist;
-  }
+}
   
 };
